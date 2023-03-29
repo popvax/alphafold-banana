21d672a54f41e2e29a1154663d3c41927e4a18da
samarth <samarth@popvax.com>
2023-03-29 07:20:23 +0000

    go

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,11 @@ FROM pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime
 WORKDIR /
 
 # Install git
-RUN apt-get update && apt-get install -y git
+RUN apt-get update && apt-get install -y git && apt-get install -y wget && apt-get install -y curl
+
+RUN curl -O https://raw.githubusercontent.com/YoshitakaMo/localcolabfold/main/install_colabbatch_linux.sh
+RUN bash install_colabbatch_linux.sh
+ENV PATH="~/colabfold_batch/colabfold-conda/bin:${PATH}"
 
 # Install python packages
 RUN pip3 install --upgrade pip
@@ -13,12 +17,6 @@ RUN pip3 install -r requirements.txt
 
 # We add the banana boilerplate here
 ADD server.py .
-
-# Add your model weight files 
-# (in this case we have a python script)
-ADD download.py .
-RUN python3 download.py
-
 
 # Add your custom app code, init() and inference()
 ADD app.py .
--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-from transformers import pipeline
-import torch
-
+import os
+import glob
+import random
+import string
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    print("Nothing to initialize")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
-    global model
+    save_path = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    os.makedirs(save_path+"_inputs", exist_ok=True)
 
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
+    fasta = f""">SEQ
+{model_inputs['sequence']}"""
 
-    # Return the results as a dictionary
-    return result
+    with open(f"{save_path}_inputs/SEQ.fasta", "w") as f:
+        f.write(fasta)
+
+    os.system(f"colabfold_batch {save_path}_inputs {save_path}")
+    result = glob.glob(f"{save_path}/*_rank_001_*.pdb")[0]
+    with open(result, "r") as f:
+        pdb = f.read()
+    os.system(f"rm -rf {save_path}*")
+    os.system(f"rm -rf {save_path}_inputs")
+    return {'pdb': pdb}
